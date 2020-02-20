1d29dc9f5bc9687d3ea7cffa619a679a18434449
Yann Lauwers <yann.lauwers@icloud.com>
2020-02-20 17:06:15 +0100

    c'est le dawa

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,15 +1,16 @@
 class DosesController < ApplicationController
 
-  def index
-    @doses = Dose.all
-  end
-
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
